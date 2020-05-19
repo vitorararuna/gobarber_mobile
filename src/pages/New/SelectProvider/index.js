5616b8b46119bc140fc60a1d2b234d1fa36039ea
vitorararuna <contato.vitorararuna@gmail.com>
2020-05-19 15:28:45 -0300

    Listagem de prestadores

--- src/pages/New/SelectProvider/index.js
+++ src/pages/New/SelectProvider/index.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
-// import { Container } from './styles';
+import Backgroud from '../../../components/Background';
 
-const New = () => {
-  return <View />;
+import api from '../../../services/api';
+
+import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
+
+export default function SelectProvider({ navigation }) {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('providers');
+
+      setProviders(response.data);
+    }
+    loadProviders();
+  }, []);
+
+  return (
+    <Backgroud>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={(provider) => String(provider.id)}
+          renderItem={({ item: provider }) => (
+            <Provider onPress={() => navigation.navigate('Date', { provider })}>
+              <Avatar
+                source={{
+                  uri: provider.avatar
+                    ? provider.avatar.url
+                    : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+                }}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
+    </Backgroud>
+  );
 }
-
-export default New;
